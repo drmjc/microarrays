f0bb490d9d0f8d28ae878756b71f6bc1d54a027c
Mark Cowley <m.cowley0@gmail.com>
2012-07-24 06:28:54 +0000

    new

---
+++ R/microarrays-package.R
@@ -0,0 +1,22 @@
+#' microarrays package title
+#' 
+#' microarrays package description
+#' 
+#' 
+#' \tabular{ll}{ 
+#' Package: \tab microarrays\cr
+#' Type: \tab Package\cr
+#' Version: \tab \Sexpr[stage=build]{packageDescription("microarrays")$Version}\cr
+#' Date: \tab \Sexpr[stage=build]{format(Sys.time(), "\%Y-\%m-\%d")}\cr
+#' License: \tab GPL-2\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#' 
+#' @name microarrays-package
+#' @aliases microarrays-package microarrays
+#' @docType package
+#' @author Mark Cowley
+#' Maintainer: Mark Cowley \email{m.cowley@@garvan.org.au}
+#' @keywords package
+#' 
+NULL

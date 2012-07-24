b18f526f021766a739d92104cf2517cb47381a95
Mark Cowley <m.cowley0@gmail.com>
2012-07-24 06:51:19 +0000

    reversed the S3 hack

--- R/as.data.frame-methods.R
+++ R/as.data.frame-methods.R
@@ -1,9 +1,7 @@
 #' @name as.data.frame.AnnotatedDataFrame
 #' @title Coerce to a Data Frame
-#' @S3method as.data.frame AnnotatedDataFrame
+#' @method as.data.frame AnnotatedDataFrame
 #' @importClassesFrom Biobase AnnotatedDataFrame
-#' @aliases as.data.frame
-#' @usage as.data.frame(x, ...)
 #' 
 #' @param x an AnnotatedDataFrame
 #' @param \dots unused

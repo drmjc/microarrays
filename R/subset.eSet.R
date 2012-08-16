ee0bbd4c1a7cd0b5049bd5a20111c8712b931d18
Mark Cowley <m.cowley0@gmail.com>
2012-08-16 04:53:15 +0000

    moved subset.LumiBatch to LumiBatch-methods

--- R/subset.eSet.R
+++ R/subset.eSet.R
@@ -29,38 +29,3 @@ subset.eSet <- function(x, subset, select, drop = FALSE, ...) {
 # 2011-09-22: bug fix, where subset was used for columns, and select for rows, instead of the opposite
 # in the standard subset function.
 # 
-
-
-
-#' subset a LumiBatch object
-#' 
-#' This is preferable to subset.eSet, since there are extra slots in a 
-#' LumiBatch
-#' 
-#' The support for subsetting the controlData is still inadequately tested.
-#' For instance, do all controlData slots have the first 2 columns being
-#' "?" and "ProbeID"?
-#' 
-#' @param x an LumiBatch
-#' @param subset logical expression indicating elements or rows to keep:
-#'          missing values are taken as \code{FALSE}.
-#' @param select logical expression, indicating columns to select from a \code{data.frame}.
-#' @param drop logical: ignored
-#' @param \dots ignored
-#' @return an LumiBatch with fewer rows and or columns
-#' @author Mark Cowley, 2011-09-01
-#' @export
-#' @method subset LumiBatch
-#' @S3method subset LumiBatch
-subset.LumiBatch <- function(x, subset, select, drop=FALSE, ...) {
-	if( missing(subset) ) subset <- rep(TRUE, nrow(x))
-	if( missing(select) ) select <- rep(TRUE, ncol(x))
-
-	# The [ and ] operators work well for eSet's already.
-	res <- x[which(subset), which(select)]
-	if(!is.null(res@controlData)) {
-		res@controlData <- subset(x@controlData,select=c(TRUE, TRUE, select))
-	}
-	
-	res
-}
